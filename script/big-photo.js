dcea14c3a9bd5d09e5c1c4018ad4e198cf903e43
hamsi <pmrt276@gmail.com>
2023-11-30 14:09:33 +0300

    3

--- script/big-photo.js
+++ script/big-photo.js
@@ -1,19 +1,33 @@
-
 window.addEventListener('load', function() {
     const menu_big_container = document.querySelector(".menu-big-photo");
     const product_photo = document.querySelector(".big-photo-img");
 
     const menus = document.querySelectorAll('.menu');
 
+    function openProductPhoto(){
+        var product_the_photo = menu.firstChild.src;
+        menu_big_container.style.display = "flex";
+        console.log(product_the_photo);
+        product_photo.src = product_the_photo;
+    }
+
+    function closeProductPhoto(){
+        openProductPhoto();
+    }
+
     menus.forEach(menu=>{
         menu.addEventListener('click', () => {
-            var product_the_photo = menu.firstChild.src;
-            menu_big_container.style.display = "flex";
-            console.log(product_the_photo);
-            product_photo.src = product_the_photo;
+            openProductPhoto();
         });
         menu_big_container.addEventListener('click', () => {
-            menu_big_container.style.display = "none";
+            closeProductPhoto();
+        });
+        menu.addEventListener("touchend", () => {
+            openProductPhoto();
+        });
+        menu_big_container.addEventListener('touchend', () => {
+            closeProductPhoto();
         });
     });
+    
 });
